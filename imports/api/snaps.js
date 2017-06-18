fdaace7f9b43a37f6c43a04101d2e264eaba5d37
Damon Kelly <damonkelly@gmail.com>
2017-06-18 14:48:22 -0400

    Display snaps from database

---
+++ imports/api/snaps.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo';
+ 
+export const Snaps = new Mongo.Collection('snaps');
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
- 
+import React, { Component, PropTypes } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+
+import { Snaps } from '../api/snaps.js';
+
 import Snap from './Snap.jsx';
  
 // App component - represents the whole app
-export default class App extends Component {
-  getSnaps() {
-    return [
-      { _id: 1, src: 'snaps/00001.jpg' },
-      { _id: 2, src: 'snaps/00002.jpg' },
-      { _id: 3, src: 'snaps/00003.jpg' },
-    ];
-  }
+class App extends Component {
+//  getSnaps() {
+//    return [
+//      { _id: 1, src: 'snaps/00001.jpg' },
+//      { _id: 2, src: 'snaps/00002.jpg' },
+//      { _id: 3, src: 'snaps/00003.jpg' },
+//    ];
+//  }
  
   renderSnaps() {
-    return this.getSnaps().map((snap) => (
+    return this.props.snaps.map((snap) => (
       <Snap key={snap._id} snap={snap} />
     ));
   }
@@ -32,3 +35,15 @@ export default class App extends Component {
     );
   }
 }
+
+App.propTypes = {
+  snaps: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+  return {
+    snaps: Snaps.find({}).fetch(),
+  };
+}, App);
+
+
